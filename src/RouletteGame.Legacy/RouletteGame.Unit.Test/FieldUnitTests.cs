25587b1095d69ad561bb236648b59873e11195ee
StefanieNielson <nielson1996@hotmail.com>
2018-02-20 09:25:19 +0100

    Added Test

--- src/RouletteGame.Legacy/RouletteGame.Unit.Test/FieldUnitTests.cs
+++ src/RouletteGame.Legacy/RouletteGame.Unit.Test/FieldUnitTests.cs
@@ -10,5 +10,12 @@ namespace RouletteGame.Unit.Test
     [TestFixture]
     class FieldUnitTests
     {
+        [Test]
+
+        public void Add()
+        {
+            Assert.That(2+2, Is.EqualTo(4));
+        }
     }
 }
+
