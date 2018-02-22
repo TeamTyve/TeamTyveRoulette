59c0ba6de1e685b5c200200cdabea803929a2b2b
Jakob Bonde Nielsen <au567214@post.au.dk>
2018-02-22 15:32:57 +0100

    Added bet tests

---
+++ src/RouletteGame.Legacy/RouletteGame.Legacy/IBet.cs
@@ -0,0 +1,10 @@
+﻿namespace RouletteGame.Legacy
+{
+    public interface IBet
+    {
+        uint Amount { get; }
+        string PlayerName { get; }
+
+        uint WonAmount(Field field);
+    }
+}
--- src/RouletteGame.Legacy/RouletteGame.Unit.Test/BetUnitTests.cs
+++ src/RouletteGame.Legacy/RouletteGame.Unit.Test/BetUnitTests.cs
@@ -4,11 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using RouletteGame.Legacy;
+using NSubstitute;
+using NSubstitute.Core;
 
 namespace RouletteGame.Unit.Test
 {
     [TestFixture]
     public class BetUnitTests
     {
+        private IBet bet;
+        private IFieldGenerator fieldGenerator; 
+
+        [SetUp]
+        public void Init()
+        {
+            bet = Substitute.For<IBet>();
+            
+            fieldGenerator = Substitute.For<IFieldGenerator>();
+            fieldGenerator.CreateFields().Returns(new List<Field> { new Field(0, color: Field.Green) });
+            
+        }
+
+        [TestCase((uint) 1)]
+        [TestCase((uint)2)]
+        [TestCase((uint)8)]
+        public void wonAmount_test_fieldbet(uint a)
+        {
+            var field = fieldGenerator.CreateFields();
+            bet = new FieldBet("testbet",a,field.First().Number);
+            Assert.That(bet.WonAmount(field.First()),Is.EqualTo(a*36));
+        }
+
+        [TestCase((uint)1)]
+        [TestCase((uint)2)]
+        [TestCase((uint)8)]
+        public void wonAmount_test_colorbet(uint a)
+        {
+            var field = fieldGenerator.CreateFields();
+            bet = new ColorBet("testbet", a, field.First().Color);
+            Assert.That(bet.WonAmount(field.First()), Is.EqualTo(a * 2));
+        }
+
+        [TestCase((uint)1)]
+        [TestCase((uint)2)]
+        [TestCase((uint)8)]
+        public void wonAmount_test_Evenoddbet(uint a)
+        {
+            var field = fieldGenerator.CreateFields();
+            bet = new EvenOddBet("testbet", a, true);
+            Assert.That(bet.WonAmount(field.First()), Is.EqualTo(a * 2));
+        }
+
+        
     }
 }
